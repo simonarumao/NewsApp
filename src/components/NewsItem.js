44551d84b63198b512a80feab61618594bac3c71
simona <simonarumao@gmail.com>
2024-06-23 00:26:43 +0530

    prev-next

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,18 +7,20 @@ export class NewsItem extends Component {
     render() {
         let { title, description,imageUrl, newsUrl } = this.props
     return (
-        <div>
-            <div className="my-3">
-            <div className="card" style={{ width: "18rem;"}}>
-                <img src={imageUrl} className="card-img-top" alt="..."/>
-                <div className="card-body">
-                    <h5 className="card-title">{title}......</h5>
-                    <p className="card-text">{ description}.....</p>
-                        <a href= {newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
-                        </div>
-                </div>
-            </div>
-        </div>
+
+        <div class="max-w-sm rounded overflow-hidden shadow-lg mb-4">
+    <img class="w-full" src={imageUrl} alt="Sunset in the mountains"/>
+    <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">{title}</div>
+        <p class="text-gray-700 text-base">{description}</p>
+    </div>
+    <div class="px-6 pt-2 pb-4">
+        <a href={newsUrl} class="text-white bg-green-800 hover:bg-green-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Read more</a>
+    </div>
+</div>
+
+
+
 
     )
   }
